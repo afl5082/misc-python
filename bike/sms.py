5732a9a8b8a1868bd4161e317c4992eceff7a03a
Adam <41493452+afl5082@users.noreply.github.com>
2021-05-27 15:59:14 -0400

    Create sms.py

---
+++ bike/sms.py
@@ -0,0 +1,24 @@
+import smtplib
+carriers = {
+	'att':    '@mms.att.net',
+	'tmobile':' @tmomail.net',
+	'verizon':  '@vtext.com',
+	'sprint':   '@page.nextel.com'
+}
+
+def send(message):
+        # Replace the number with your own, or consider using an argument\dict for multiple people.
+	to_number = '0000000000{}'.format(carriers['att'])
+	auth = ('**email**', '**password**')
+
+	# Establish a secure session with gmail's outgoing SMTP server using your gmail account
+	server = smtplib.SMTP( "smtp.gmail.com", 587 )
+	server.starttls()
+	server.login(auth[0], auth[1])
+
+	# Send text message through SMS gateway of destination number
+	server.sendmail( auth[0], to_number, message)
+
+
+
+#send('do you want any custom alerts ? love you')
